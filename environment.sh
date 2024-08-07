48d673e6afd873aa458fb67a93b9717399880f96
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2024-08-07 16:28:20 +0100

    Init beamline repo

---
+++ environment.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# a bash script to source in order to set up your command line to in order
+# to work with the bl01t IOCs and Services.
+
+# check we are sourced
+if [ "$0" = "$BASH_SOURCE" ]; then
+    echo "ERROR: Please source this script"
+    exit 1
+fi
+
+echo "Loading environment for beamline bl01t IOC Instances and Services ..."
+
+#### SECTION 1. Environment variables ##########################################
+
+export EC_CLI_BACKEND="K8S"
+# the namespace to use for kubernetes deployments
+export EC_TARGET=b01-1-beamline
+# the git repo for this project
+export EC_SERVICES_REPO=git@github.com:marcelldls/bl01t.git
+# declare your centralised log server Web UI
+export EC_LOG_URL="https://graylog2.diamond.ac.uk/search?rangetype=relative&fields=message%2Csource&width=1489&highlightMessage=&relative=172800&q=pod_name%3A{service_name}*"
+
+#### SECTION 2. Install ec #####################################################
+
+# check if epics-containers-cli (ec command) is installed
+if ! ec --version &> /dev/null; then
+    echo "ERROR: Please set up a virtual environment and: 'pip install edge-containers-cli'"
+    return 1
+fi
+
+# enable shell completion for ec commands
+source <(ec --show-completion ${SHELL})
+
+
+#### SECTION 3. Configure Kubernetes Cluster ###################################
+
+
+# the following configures kubernetes inside DLS.
+
+module unload k8s-b01-1 > /dev/null
+module load k8s-b01-1 > /dev/null
+# set the default namespace for kubectl and helm (for convenience only)
+kubectl config set-context --current --namespace=b01-1-beamline
+# make sure the user has provided credentials
+kubectl version
+
+
+# enable shell completion for k8s tools
+if [ -n "$ZSH_VERSION" ]; then SHELL=zsh; fi
+source <(helm completion $(basename ${SHELL}))
+source <(kubectl completion $(basename ${SHELL}))
